36957874a4beb775c07fa99db1c03da8b848a4ce
Priyansh <youname972@gmail.com>
2022-01-01 01:25:54 -0500

    Added Deck Class

--- UnoCards.py
+++ UnoCards.py
@@ -1,4 +1,8 @@
 #Card class and constructor 
+#Normal cards - 76 Numbered cards of 4 suits from 0-9
+#Action cards - 8 2+ , 8 Skips, 8 Change
+#Wild Cards - 4 4+ and 4 Color Change  
+
 suits = ["Red", "Yellow", "Blue", "Green"]
 
 class Cards:
@@ -6,8 +10,24 @@ class Cards:
         self.suit = suit
         self.value = value
 
-    def playWild(self):
-        if self.suit == "Wild":
+    def checkLegal(self, obj):
+        if self.value == obj.value:
+            return True
+        elif self.suit == obj.value:
+            return True
+        elif self.suit == "Wild" or self.suit == "4Plus":
+            return True
+        else:
+            print("Please play a legal move")
+            return False
+
+    def playPlusCards(self, player):
+        if self.value == -2 or self.suit == "4Plus":
+            player.draw()
+        
+
+    def playColorChange(self):
+        if self.suit == "Wild" or self.suit == "4Plus":
             c = int(input("""Choose the new color of the pile:
                                 1. Red
                                 2. Yellow
@@ -15,32 +35,23 @@ class Cards:
                                 4. Green"""))
             self.value = suits[c-1]
 
-    def checkLegal(self, obj):
-        if self.value == obj.value:
-            return True
-        elif self.suit == obj.value:
-            return True
-        else:
-            return False
+ 
             
 
 #Loop for creating cards
-#Normal cards - 76 Numbered cards of 4 suits from 0-9
-#Action cards - 8 2+ , 8 Skips, 8 Change
-#Wild Cards - 4 4+ and 4 Color Change           
 def createDeck():
     for i in suits:
-        for r in range(0,10):  #Num cards
+        for r in range(0,10):               #Num cards
             Cards(i,r)
 
 
-        for r in range(0,3):    #Action cards
-            for j in range(0,2):
-                Cards(i, -1-r)
+        for r in range(0,3):                #Action cards
+            for j in range(0,2):                #-2 for 2+ | -3 for Skip | -4 for Direction
+                Cards(i, -1-r)  
+    
+        Cards("4Plus")                      #4+ cards
 
-        Cards(i,-4)             #4+ cards
-
-        Cards("Wild")             #Wild
+        Cards("Wild")                       #Wild
 
 
     
